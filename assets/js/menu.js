1c78263e737f3e57342e2a6c20ce42beada7882b
MiaRasmussen05 <rasmussenm376@gmail.com>
2022-11-10 10:00:45 +0000

    Add and link up menu js for settings to open and close it

---
+++ assets/js/menu.js
@@ -0,0 +1,12 @@
+let setting = document.getElementById("settings")
+
+/**
+ * Open and close the settings pop up window
+ */
+ function openSettings() {
+    setting.style.display = "block";
+};
+
+function closeSettings() {
+    setting.style.display = "none";
+};
