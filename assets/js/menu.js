14a6b44edf9f82abc8aab9703a4de65875afe4e2
MiaRasmussen05 <rasmussenm376@gmail.com>
2022-11-10 10:31:34 +0000

    Add js function to open and close menu

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,4 +1,5 @@
 let setting = document.getElementById("settings")
+let toggle = document.getElementById("toggleMenu")
 
 /**
  * Open and close the settings pop up window
@@ -9,4 +10,15 @@ let setting = document.getElementById("settings")
 
 function closeSettings() {
     setting.style.display = "none";
+};
+
+/**
+ * Open and close the hamburger menu on smaller screens
+ */
+ function openToggleMenu() {
+    toggle.style.display = "block";
+};
+
+function closeToggleMenu() {
+    toggle.style.display = "none";
 };
