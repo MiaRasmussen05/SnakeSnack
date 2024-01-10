f9c8fbe8140094c70c0a944a11c340c1f58e5791
MiaRasmussen05 <rasmussenm376@gmail.com>
2024-01-10 20:08:51 +0000

    Add pause message with style

--- assets/js/pause.js
+++ assets/js/pause.js
@@ -4,15 +4,30 @@ const pauseBtn = document.getElementById('pauseBtn');
 // Variable to store the previous speed before pausing
 let previousSpeed = regularSpeed;
 
+let isPauseDisplayed = false;
+
+const pauseMessage = document.createElement('div');
+pauseMessage.classList.add('pause-message');
+document.body.appendChild(pauseMessage);
+
+function displayPauseMessage(pause) {
+    pauseMessage.innerText = `Pause`;
+    isPauseDisplayed = true;
+
+    pauseMessage.classList.add('show-pause');
+}
+
 // Event listener for the pause button click
 pauseBtn.addEventListener('click', () => {
     if (SPEED !== 0) {
         previousSpeed = SPEED;
         SPEED = 0;
         pauseBtn.innerHTML = '<i class="fas fa-play"></i>';
+        displayPauseMessage(true);
     } else {
-        
         SPEED = previousSpeed;
         pauseBtn.innerHTML = '<i class="fas fa-pause"></i>';
+        isPauseDisplayed = false;
+        pauseMessage.classList.remove('show-pause');
     }
 });
