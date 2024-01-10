ff2811cedf9a35ab563c1ab423a3734200cf7ac6
MiaRasmussen05 <rasmussenm376@gmail.com>
2024-01-10 21:06:27 +0000

    Fix js for close outside menus for settings and hamburger menu

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,24 +1,52 @@
 let setting = document.getElementById("settings");
 let toggle = document.getElementById("toggleMenu");
+let settingsBtn = document.getElementById("settingsBtn");
+let menuBtn = document.getElementById("menuBtn");
+let settingsOverlay = document.querySelector(".overlay");
+let overlayMenu = document.querySelector(".overlayMenu");
+
+/**
+ * Event to close the settings and menu by clicking outside 
+ * the settings or hamburger menu container
+ */
+window.onclick = function (event) {
+    if (event.target == settingsOverlay) {
+        setting.style.display = "none";
+        settingsOverlay.style.display = "none";
+    }
+
+    if (event.target == overlayMenu) {
+        toggle.style.display = "none";
+        overlayMenu.style.display = "none";
+    }
+};
 
 /**
  * Open and close the settings pop up window
  */
  function openSettings() {
     setting.style.display = "block";
+    settingsOverlay.style.display = "block";
 }
 
 function closeSettings() {
     setting.style.display = "none";
+    settingsOverlay.style.display = "none";
 }
 
 /**
  * Open and close the hamburger menu on smaller screens
  */
- function openToggleMenu() {
+function openToggleMenu() {
     toggle.style.display = "block";
+    overlayMenu.style.display = "block";
 }
 
 function closeToggleMenu() {
     toggle.style.display = "none";
+    overlayMenu.style.display = "none";
 }
+
+
+settingsBtn.addEventListener("click", openSettings);
+settingsOverlay.addEventListener("click", openToggleMenu);
