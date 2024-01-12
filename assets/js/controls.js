2d11b8d3edc50b7e2b137e8bc3b13b1b17069769
MiaRasmussen05 <rasmussenm376@gmail.com>
2024-01-12 22:11:37 +0000

    Fix control buttons

--- assets/js/controls.js
+++ assets/js/controls.js
@@ -1,80 +1,38 @@
-const controlButtons = document.getElementsByClassName('touch-controls')[0];
 const touchControls = document.getElementsByClassName('btnControls');
 
-// Event listener for keydown to control the input of moving the snake
-window.addEventListener("keydown", e => {
-    switch (e.key) {
-        case "ArrowUp":
-            if (lastDirection.y !== 0) 
-            break;
+function touchControlsClicked() {
+    if (this.getAttribute("id") === "upBtn") {
+        if (lastDirection.y !== 0) {
+            return;
+        } else {
             direction = {
                 x: 0,
                 y: -1
             };
-            break;
-        case "ArrowLeft":
-            if (lastDirection.x !== 0) 
-            break;
-            direction = {
-                x: -1,
-                y: 0
-            };
-            break;
-        case "ArrowRight":
-            if (lastDirection.x !== 0) 
-            break;
+            return;
+        }
+    } else if (this.getAttribute("id") === "rightBtn") {
+        if (lastDirection.x !== 0) {
+            return;
+        } else {
             direction = {
                 x: 1,
                 y: 0
             };
-            break;
-        case "ArrowDown":
-            if (lastDirection.y !== 0) 
-            break;
-            direction = {
-                x: 0,
-                y: 1
-            };
-            break;
-    }
-});
-
-/**
- * Function to get the attribute for the game controls on the game page to move the snake
- */
-function touchControlsClicked() {
-    if (this.getAttribute("id") === "upBtn") {
-        if(lastDirection.y !== 0){
-         return;
-        } else {
-         direction = {
-             x: 0,
-             y: -1
-            };
             return;
         }
-    } else if (this.getAttribute("id") === "rightBtn") {
-       if(lastDirection.x !== 0){
-        return;
-       } else {
-        direction = {
-            x: 1,
-            y: 0
-        };
-        return;
-       }
     } else if (this.getAttribute("id") === "leftBtn") {
-        if(lastDirection.x !== 0){
+        if (lastDirection.x !== 0) {
             return;
         } else {
             direction = {
                 x: -1,
                 y: 0
-        };
-        return;
-       }
+            };
+            return;
+        }
     } else if (this.getAttribute("id") === "downBtn") {
-        if(lastDirection.y !== 0){
+        if (lastDirection.y !== 0) {
             return;
         } else {
             direction = {
@@ -84,4 +42,8 @@ function touchControlsClicked() {
             return;
         }
     }
+}
+
+for (let i = 0; i < touchControls.length; i++) {
+    touchControls[i].addEventListener('click', touchControlsClicked);
 }
