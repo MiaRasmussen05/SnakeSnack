114812d0ebc7a968668c96425ed8c8700f67b3c7
MiaRasmussen05 <rasmussenm376@gmail.com>
2024-01-12 17:03:50 +0000

    Add so direction so the snake can not go back into it self

--- assets/js/touchscreen.js
+++ assets/js/touchscreen.js
@@ -24,20 +24,20 @@ function handleTouchMove(event) {
     const threshold = 3;
 
     if (Math.abs(deltaX) > Math.abs(deltaY)) {
-        // Horizontal swipe
-        if (deltaX > threshold) {
+        // Horizontal swipe "X"
+        if (deltaX > threshold && lastDirection.x !== -1) {
             // Swipe right
             direction = { x: 1, y: 0 };
-        } else if (deltaX < -threshold) {
+        } else if (deltaX < -threshold && lastDirection.x !== 1) {
             // Swipe left
             direction = { x: -1, y: 0 };
         }
     } else {
-        // Vertical swipe
-        if (deltaY > threshold) {
+        // Vertical swipe "Y"
+        if (deltaY > threshold && lastDirection.y !== -1) {
             // Swipe down
             direction = { x: 0, y: 1 };
-        } else if (deltaY < -threshold) {
+        } else if (deltaY < -threshold && lastDirection.y !== 1) {
             // Swipe up
             direction = { x: 0, y: -1 };
         }
