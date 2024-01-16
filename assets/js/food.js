f0ccb1e0f7dbb9e99389ea0492952981370623da
MiaRasmussen05 <rasmussenm376@gmail.com>
2024-01-16 20:21:53 +0000

    Fix food logic with food clearout with bad and good food

--- assets/js/food.js
+++ assets/js/food.js
@@ -1,10 +1,12 @@
 let food = getARandomFoodPosition();
-// The amount the snake expans by every food it eats
+let foodTimeout; 
+
+// The amount the snake expands by every food it eats
 const EXPANSION = 1;
 
+let isGoodFood = true; // Variable to track the type of food
 let score = 0;
 const getScore = document.getElementsByClassName('score')[0];
-
 
 /**
  * Gets the style from the css to create the element of the food
@@ -13,20 +15,67 @@ const getScore = document.getElementsByClassName('score')[0];
 function drawFood(board) {
     let theFoodElement = document.createElement('div');
     theFoodElement.classList.add('food');
+
+    // Check the type of food based on the variable isGoodFood
+    if (isGoodFood) {
+        // Good food
+        theFoodElement.classList.add('good-food');
+    } else {
+        // Bad food
+        theFoodElement.classList.add('bad-food');
+    }
+
     board.appendChild(theFoodElement);
     theFoodElement.style.gridColumnStart = food.x;
     theFoodElement.style.gridRowStart = food.y;
 }
+
+function determineFoodType() {
+    const randomValue = Math.random();
+
+    // Check the type of food based on the random value
+    if (randomValue < 0.9) {
+        // 90% chance for good food
+        isGoodFood = true;
+    } else {
+        // 10% chance for bad food
+        isGoodFood = false;
+    }
+}
+
 
 /**
  * Update new food position and get score from expansion of the snake
  */
 function updateFood() {
     if (onTheSnake(food)) {
-        expand(EXPANSION);
-        score += 9;
+        if (isGoodFood) {
+            // Good food
+            expand(EXPANSION);
+            score += 9;
+        } else {
+            // Bad food
+            score += -5;
+        }
+
         food = getARandomFoodPosition();
+        determineFoodType();
+        resetFoodTimeout();
     }
+}
+
+function drawNewFood() {
+    food = getARandomFoodPosition();
+    determineFoodType();
+    drawFood(board);
+    resetFoodTimeout();
+}
+
+function resetFoodTimeout() {
+    clearTimeout(foodTimeout);
+    foodTimeout = setTimeout(() => {
+        drawNewFood();
+    }, 7000);
 }
 
 /**
